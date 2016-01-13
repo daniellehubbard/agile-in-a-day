e99184090f6781a5c93a4f8af916704f5d7b6936
Hubbard, Danielle (DS) <Danielle.Hubbard@adp.com>
2016-01-13 07:26:50 -0800

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.tpls', 'ui.bootstrap.transition', 'myApp.controllers', 'myApp.filters', 'myApp.services', 'myApp.directives', 'firebase']).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/home', {templateUrl: 'app/partials/home.html', controller: 'HomeCtrl'});
+        $routeProvider.when('/contactus', {templateUrl: 'app/partials/contactus.html', controller: 'ContactUsCtrl'});
+        $routeProvider.when('/vehicles', {templateUrl: 'app/partials/vehicles.html', controller: 'VehiclesCtrl'});
+        $routeProvider.when('/contactme', {templateUrl: 'app/partials/contactme.html', controller: 'ContactMeCtrl'});
+        $routeProvider.when('/featured', {templateUrl: 'app/partials/featured.html', controller: 'FeaturedCtrl'});
+        $routeProvider.when('/offers', {templateUrl: 'app/partials/offers.html', controller: 'OffersCtrl'});
+        $routeProvider.otherwise({redirectTo: '/home'});
+    }])
+    .constant('CONFIGURL', 'https://chick-tech-cdk.firebaseio.com')
+    .factory('getVehiclesFirebaseStore', ['$firebase', 'CONFIGURL', function($firebase, CONFIGURL) {
+        var firebaseStore;
+        return function(msg) {
+            if(!firebaseStore) {
+                var ref = new Firebase(CONFIGURL + '/Vehicles');
+                var sync = $firebase(ref);
+                firebaseStore = sync.$asArray();
+            }
+            return firebaseStore;
+        };
+    }])
+    .factory('getChatFirebaseStore', ['$firebase', 'CONFIGURL', function($firebase, CONFIGURL) {
+        var firebaseStore;
+        return function(msg) {
+            if(!firebaseStore) {
+                firebaseStore = new Firebase(CONFIGURL + '/Chat');
+            }
+            return firebaseStore;
+        };
+    }]);
