abfba3b07335ac07b8ba7f3a18195d61b4ea3467
Hensley, Michael (DS) <Michael.Hensley@cdk.com>
2016-01-18 14:29:26 -0800

    Adding in contact info

--- app/js/controllers/ContactMeController.js
+++ app/js/controllers/ContactMeController.js
@@ -6,17 +6,30 @@ angular.module('myApp.controllers')
         navService.setActiveTab("contactme");
 
         $scope.contactinfo = {
-            FirstName: ''
+            Name: '',
+            PhoneNumber:'',
+            Email:'',
+            Comments:'',
+            Birthdate:'',
+            Gender:''
         };
         $scope.contactMe = function(widget){
+            console.log(widget);
+            var copy = angular.copy(widget);
             var modalInstance = $modal.open({
                 templateUrl: 'app/partials/modalcontent.html',
                 controller: 'ModalInstanceCtrl',
                 resolve: {
                     widget: function() {
-                        return widget;
+                        return copy;
                     }
                 }
             });
+            $scope.contactinfo.Name= '';
+            $scope.contactinfo.PhoneNumber= '';
+            $scope.contactinfo.Email= '';
+            $scope.contactinfo.Comments= '';
+            $scope.contactinfo.Birthdate= '';
+            $scope.contactinfo.Gender= '';
         };
     }]);
